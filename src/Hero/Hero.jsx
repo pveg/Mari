9432af4c4e072231394f3c37f6dbf17ef82f4f62
Paulo Torres <pveg@live.com>
2022-10-18 12:39:57 +0100

    herojsx formatting

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -9,27 +9,35 @@ function Hero() {
     <section ref={ref}>
       <div
         className="hero min-h-screen z-0"
-        style={{ backgroundImage: `url("https://res.cloudinary.com/daknbj7xw/image/upload/v1666090903/Mari/background_vnj2e1.png")` }}
+        style={{
+          backgroundImage: `url("https://res.cloudinary.com/daknbj7xw/image/upload/v1666090903/Mari/background_vnj2e1.png")`,
+        }}
       >
         <div className="hero-overlay bg-opacity-40">
-       <img style={{
-          transform: isInView ? "none" : "translateY(-100px)",
-          opacity: isInView ? 1 : 0,
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-        }} 
-        className="z-10 w-full mt-28" src="https://res.cloudinary.com/daknbj7xw/image/upload/v1666090903/Mari/BR_dwvweq.png" alt="" />
+          <img
+            style={{
+              transform: isInView ? "none" : "translateY(-100px)",
+              opacity: isInView ? 1 : 0,
+              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            }}
+            className="z-10 w-full mt-28"
+            src="https://res.cloudinary.com/daknbj7xw/image/upload/v1666090903/Mari/BR_dwvweq.png"
+            alt=""
+          />
         </div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 style={{
-          transform: isInView ? "none" : "translateY(+200px)",
-          opacity: isInView ? 1 : 0,
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-        }} 
-        className="mb-5 text-3xl font-bold">Learn Portuguese with Mariana</h1>
-            <p className="mb-5">
-              The most dynamic way of learning portuguese
-            </p>
+            <h1
+              style={{
+                transform: isInView ? "none" : "translateY(+200px)",
+                opacity: isInView ? 1 : 0,
+                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+              }}
+              className="mb-5 text-3xl font-bold"
+            >
+              Learn Portuguese with Mariana
+            </h1>
+            <p className="mb-5">The most dynamic way of learning portuguese</p>
             <button className="btn btn-outline">Why Portuguese?</button>
           </div>
         </div>
